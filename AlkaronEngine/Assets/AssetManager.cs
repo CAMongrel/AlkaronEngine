6ac95e4b2816e2b0ffd2946289b9f7aa6af50636
Henning Thoele <camongrel@gmail.com>
2019-02-01 18:31:34 +0100

    WIP integration of Hellspawn Enginge

---
+++ AlkaronEngine/Assets/AssetManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AlkaronEngine.Assets
+{
+	public class AssetManager
+	{
+		#region Load<T>
+		public T Load<T>(string FullAssetName) where T : Asset, new()
+		{
+			if (FullAssetName == null)
+            {
+                return null;
+            }
+
+            string[] assetPath = FullAssetName.Split('.');
+			if (assetPath.Length != 3)
+			{
+				// TODO!!!
+				//Log.LogError("Invalid name specified for Load Asset.");
+				return new T();
+			}
+			
+			Package pkg = AlkaronCoreGame.Core.PackageManager.LoadPackage(assetPath[0]);
+			if (pkg == null)
+            {
+                return new T();
+            }
+
+            T asset = pkg.GetAsset(assetPath[1] + "." + assetPath[2]) as T;
+			if (asset == null)
+            {
+                return new T();
+            }
+
+            return asset;
+		}
+		#endregion		
+
+		#region GetAssetNamesByType
+		/// <summary>
+		/// Returns a list of all assets of the specified type (e.g. "Surface2D")
+		/// in the package specified by "packageToSearch".
+		/// "packageToSearch" can be null in which case all known/loaded packages
+		/// will be searched.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="packageToSearch"></param>
+		/// <returns></returns>
+		public string[] GetAssetNamesByType(string type, 
+			Package packageToSearch, bool excludePackage)
+		{
+			if (packageToSearch != null &&
+				excludePackage == false)
+            {
+                return InternalGetAssetNamesByType(type, packageToSearch);
+            }
+
+            List<string> resultList = new List<string>();
+			foreach (KeyValuePair<string, Package> pair in AlkaronCoreGame.Core.PackageManager.LoadedPackages)
+			{
+				if (pair.Value == packageToSearch)
+                {
+                    continue;
+                }
+
+                resultList.AddRange(InternalGetAssetNamesByType(
+					type, pair.Value));
+			}
+
+			resultList.Sort();
+
+			return resultList.ToArray();
+		}
+
+		/// <summary>
+		/// Internal helper for GetAssetNamesByType.
+		/// This function does the actual searching of a package.
+		/// </summary>
+		private static string[] InternalGetAssetNamesByType(string type,
+			Package packageToSearch)
+		{
+			List<string> resultList = new List<string>();
+
+			Asset[] assetList = packageToSearch.GetAssetsByType(type);
+			for (int i = 0; i < assetList.Length; i++)
+			{
+				resultList.Add(assetList[i].Fullname);
+			}
+
+			return resultList.ToArray();
+		}
+		#endregion
+
+		#region GetOriginalFilename
+		/// <summary>
+		/// Returns the original filename of the asset from which it
+		/// was imported. Returns null if any error happens.
+		/// </summary>
+		public static string GetOriginalFilename(string AssetName)
+		{
+			string fullAssetname = Path.Combine(
+				AlkaronCoreGame.Core.ContentDirectory, AssetName);
+
+			if (File.Exists(fullAssetname) == false)
+            {
+                return null;
+            }
+
+            string result = null;
+			using (BinaryReader reader = 
+				new BinaryReader(File.OpenRead(fullAssetname)))
+			{
+				try
+				{
+					result = reader.ReadString();
+				}
+				catch
+				{
+					result = null;
+				}
+			}
+
+			if (result == null ||
+				File.Exists(result) == false)
+            {
+                return null;
+            }
+
+            return result;
+		}
+		#endregion
+	}
+}
---
+++ AlkaronEngine/Assets/Materials/Surface2D.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.IO;
+
+namespace AlkaronEngine.Assets.Materials
+{
+	/// <summary>
+	/// Surface 2D
+	/// </summary>
+	public class Surface2D : Asset
+	{
+        /// <summary>
+        /// Texture
+        /// </summary>
+        /// <returns>Texture 2D</returns>
+        public Texture2D Texture { get; private set; } // Texture
+
+        /// <summary>
+        /// Is valid surface?
+        /// </summary>
+        public override bool IsValid
+        {
+            get
+            {
+                return Texture != null;
+            } // get
+        } // IsValid
+
+        public const int MaxAssetVersion = 2;
+
+        /// <summary>
+        /// Texture filter minimum
+        /// </summary>
+        /// <returns>Texture filter</returns>
+        public TextureFilter TextureFilterMin { get; set; } // TextureFilterMin
+		/// <summary>
+		/// Texture filter mag
+		/// </summary>
+		/// <returns>Texture filter</returns>
+		public TextureFilter TextureFilterMag { get; set; } // TextureFilterMag
+		/// <summary>
+		/// Texture filter mip
+		/// </summary>
+		/// <returns>Texture filter</returns>
+		public TextureFilter TextureFilterMip { get; set; } // TextureFilterMip
+
+        /// <summary>
+        /// Mip start
+        /// </summary>
+        private int[] mipStart;
+        /// <summary>
+        /// Mip size
+        /// </summary>
+        private int[] mipSize;
+		/// <summary>
+		/// Raw data
+		/// </summary>
+		private byte[] rawData;
+
+		/// <summary>
+		/// Create surface 2D
+		/// </summary>
+		public Surface2D()
+		{
+            Texture = null;
+		} // Surface2D()
+
+		/// <summary>
+		/// Dispose
+		/// </summary>
+		public override void Dispose()
+		{
+			if (Texture != null)
+            {
+                Texture.Dispose();
+            }
+        } // Dispose()
+
+		#region Load
+		/// <summary>
+		/// Loads a binary surface 2D
+		/// </summary>
+		public override void Load(string packageName, string assetName,
+			Stream stream)
+		{
+			Name = assetName;
+			PackageName = Path.GetFileNameWithoutExtension(packageName);
+
+			BinaryReader reader = new BinaryReader(stream);
+			{
+				string magic = reader.ReadString();
+				AssetVersion = reader.ReadInt32();
+
+				OriginalFilename = reader.ReadString();
+
+				int Width = reader.ReadInt32();
+				int Height = reader.ReadInt32();
+                int MipLevels = reader.ReadInt32();
+				SurfaceFormat format = (SurfaceFormat)reader.ReadInt32();
+
+				if (AssetVersion > 1)
+				{
+					TextureFilterMin = (TextureFilter)reader.ReadInt32();
+					TextureFilterMag = (TextureFilter)reader.ReadInt32();
+					TextureFilterMip = (TextureFilter)reader.ReadInt32();
+				} // if (assetVersion)
+
+                bool autoGenerateMipmaps = (MipLevels < 2);
+
+				int count = reader.ReadInt32();
+				rawData = reader.ReadBytes(count);
+
+				try
+				{
+                    Texture2D newDXTexture = new Texture2D(AlkaronCoreGame.Core.GraphicsDevice,
+                        Width, Height, autoGenerateMipmaps, format);
+					if (newDXTexture != null)
+					{
+						// Dispose old texture
+						if (Texture != null)
+						{
+                            Texture.Dispose();
+                            Texture = null;
+						} // if (dxTexture)
+
+                        Texture = newDXTexture;
+					} // if (newDXTexture)
+				} // try
+				catch (Exception ex)
+				{
+					AlkaronCoreGame.Core.Log("Failed to load Surface2D:\r\n" + ex);
+					return;
+				} // catch
+
+				mipStart = new int[MipLevels];
+				mipSize = new int[MipLevels];
+				for (int i = 0; i < MipLevels; i++)
+				{
+					mipStart[i] = reader.ReadInt32();
+					mipSize[i] = reader.ReadInt32();
+
+                    Texture.SetData(i, null, rawData, mipStart[i], mipSize[i]);
+				} // for (int)
+			} // block
+		} // Load(packageName, assetName, stream)
+		#endregion
+
+		#region Save
+		/// <summary>
+		/// 
+		/// </summary>
+		public override void Save(BinaryWriter writer)
+		{
+			writer.Write("HAF ");
+			writer.Write(MaxAssetVersion);
+			writer.Write(OriginalFilename);
+
+			writer.Write(Texture.Width);
+			writer.Write(Texture.Height);
+			writer.Write(Texture.LevelCount);
+			writer.Write((int)Texture.Format);
+
+			writer.Write((int)TextureFilterMin);
+			writer.Write((int)TextureFilterMag);
+			writer.Write((int)TextureFilterMip);
+
+			writer.Write(rawData.Length);
+			writer.Write(rawData);
+
+			for (int i = 0; i < Texture.LevelCount; i++)
+			{
+				writer.Write(mipStart[i]);
+				writer.Write(mipSize[i]);
+			} // for (int)
+        } // Save(writer)
+        #endregion
+	} // class Surface2D
+} // namespace HellspawnEngine.Assets.Materials
